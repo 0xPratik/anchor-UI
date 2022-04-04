4d6c582a301a785bae8c998e0406b9a2ff97477e
ProPratik1405 <sariapratik@gmail.com>
2022-04-04 15:06:10 +0530

    Form UI added Wallet Connection Done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,43 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { FC, useMemo } from "react";
+
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const endpoint = "https://api.devnet.solana.com";
+
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+  ];
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Component {...pageProps} />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </ChakraProvider>
   );
 }
